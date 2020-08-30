b84fb1b74c15d7f5f17d2e3b14bb3c6ebf88aff8
takayama-kento <66887879+takayama-kento@users.noreply.github.com>
2020-08-31 02:12:59 +0900

    ツイート一覧画面

---
+++ Laravel/resources/js/components/Tweet.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="col-md-8 mb-3">
+            <div class="card">
+                <div class="card-header p-3 w-100 d-flex">
+                    <img src="https://drive.google.com/uc?id=1tRwOMX-PoWhp1dFhuV2wpe5_cOc6699W" class="rounded-circle" width="50" height="50">
+                    <div class="ml-2 d-flex flex-column flex-grow-1">
+                        <RouterLink
+                            class="text-secondary"
+                            :to="`/users/${item.author.id}`"
+                        >
+                            {{ item.author.name }}
+                        </RouterLink>
+                    </div>
+                    <div class="d-flex justify-content-end flex-grow-1">
+                        <p class="mb-0 text-secondary">
+                            {{ 作成日 }}
+                        </p>
+                    </div>
+                </div>
+                <div class="card-body">
+                    {{ item.tweet }}
+                </div>
+                <div class="card-footer py-1 justify-content-end bg-white">
+                    <div class="mr-2 d-flex align-items-center">
+                        <button class="user__action user__action--like">
+                            <i class="icon icon-md-heart"></i>{{ いいねの数 }}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    },
+}
+</script>
--- Laravel/resources/js/pages/TweetList.vue
+++ Laravel/resources/js/pages/TweetList.vue
@@ -1,3 +1,45 @@
 <template>
-    <h1>ツイート一覧</h1>
+    <div class="tweet-list">
+        <Tweet
+            v-for="tweet in tweets"
+            :key="tweet.id"
+            :item="tweet"
+        />
+    </div>
 </template>
+
+<script>
+import { OK } from '../util'
+import Tweet from '../components/Tweet.vue'
+
+export default {
+    components: {
+        Tweet
+    },
+    data () {
+        return {
+            tweets: []
+        }
+    },
+    methods: {
+        async fetchTweets () {
+            const response = await axios.get('/api/tweets')
+
+            if (response.status !== OK) {
+                this.$store.commit('error/setCode', response.status)
+                return false
+            }
+
+            this.tweets = response.data.data
+        }
+    },
+    watch: {
+        $route: {
+            async handler() {
+                await this.fetchTweets()
+            },
+            immediate: true
+        }
+    }
+}
+</script>
